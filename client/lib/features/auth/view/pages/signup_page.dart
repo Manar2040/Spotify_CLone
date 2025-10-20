2ba30f93259d773180cca7df276ef4f1aba5eae4
Manar2040 <manar20402004@gmail.com>
2025-10-20 17:35:00 +0300

    Sign up UI

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,5 +1,7 @@
+import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/auth_gadient_button.dart';
 import '../widgets/custom_field.dart';
 
 class SignupPage extends StatefulWidget {
@@ -10,29 +12,64 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+    formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-                    'Sign Up.',
+        child: Form(
+          key:formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                      'Sign Up.',
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      )
+                      ),
+               const SizedBox(height: 15,),       
+               CustomField( hintText: 'Name',controller: nameController,),
+               const SizedBox(height: 15,), 
+               CustomField(hintText: 'Email',controller: emailController,),
+               const SizedBox(height: 15,),
+               CustomField(hintText: 'Password',controller: passwordController,isObscureText: true,),
+               const SizedBox(height: 20,),
+               AuthGadientButton(),
+               const SizedBox(height: 20,),
+               RichText(text: TextSpan(
+                text: 'Already have an account? ',
+                style: Theme.of(context).textTheme.titleMedium,
+                children: [
+                  TextSpan(
+                    text: 'Sign in',
                     style: TextStyle(
-                      fontSize: 50,
+                      color: Pallete.gradient2,
                       fontWeight: FontWeight.bold,
-                    )
                     ),
-             const SizedBox(height: 15,),       
-             CustomField( hintText: 'Name',),
-             const SizedBox(height: 15,), 
-             CustomField(hintText: 'Email',),
-             const SizedBox(height: 15,),
-             CustomField(hintText: 'Password',),
-          ],
+                  ),
+                ],
+               ),)
+            ],
+            
+          ),
         ),
       ),
     );
